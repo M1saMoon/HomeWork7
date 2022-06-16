6aa3f9d475fa2dcdcb0605d84bf86be5ca6b474e
Mira <5555159m@gmail.com>
2022-06-16 22:25:42 +0300

    add 1 task

--- 01_Exchange/Program.cs
+++ 01_Exchange/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая обменивает элементы первой строки и последней строки
+
+int [,] Array = new int [3,5];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Array[i,j] = new Random().Next(50);
+        Console.Write(Array[i,j] + " ");
+    }
+Console.WriteLine();
+}
+int [,] Revers(int [,] Matrix)
+{
+    int temp = 0;
+    for (int j = 0; j < Matrix.GetLength(1); j++)
+    {
+       temp = Matrix[0,j];
+       Matrix[0,j] = Matrix[Matrix.GetLength(0)-1,j];
+       Matrix[Matrix.GetLength(0)-1,j] = temp;
+    }
+    return Matrix;
+}
+Console.WriteLine();
+void PrintArray(int [,] Matrix)
+{
+    for (int i = 0; i < Matrix.GetLength(0);i++)
+    {
+        for (int j = 0; j < Matrix.GetLength(1); j++)
+        {
+            Console.Write(Matrix[i,j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+PrintArray(Revers(Array));
